d0b2e892f738832143f5a736c2f8e538e9fda673
AlfredStevenLee <36948749+AlfredStevenLee@users.noreply.github.com>
2018-03-12 16:30:14 +0900

    commit

--- router/mainrouter.js
+++ router/mainrouter.js
@@ -6,4 +6,7 @@ module.exports = function(app)
   app.get('/about', function(req, res){
     res.render('about.html');
   });
+  app.get('/sotmain', function(req, res){
+    res.render('../sot_main.html');
+  });
 }
